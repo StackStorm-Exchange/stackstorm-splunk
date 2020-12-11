47ecfad6f7864d0990f95c019d5439a378413f9b
Marcelo Arakaki <marcelo.arakaki@gmail.com>
2020-12-10 21:11:17 -0300

    linter corrections

--- actions/send_log.py
+++ actions/send_log.py
@@ -1,24 +1,30 @@
 import requests
 import json
+
 from st2common.runners.base_action import Action
+
+__all__ = [
+    'SendLogAction'
+]
+
 
 class SendLogAction(Action):
     def __init__(self, config):
         super(SendLogAction, self).__init__(config)
 
-        host=self.config.get('host')
-        port=self.config.get('hec_port')
-        scheme=self.config.get('scheme')
-        endpoint=self.config.get('hec_endpoint')
-        self.url=scheme + '://' + host + ':' + port + endpoint
+        host = self.config.get('host')
+        port = self.config.get('hec_port')
+        scheme = self.config.get('scheme')
+        endpoint = self.config.get('hec_endpoint')
+        self.url = scheme + '://' + host + ':' + port + endpoint
 
     def run(self, index, token, event):
         event_headers = {'Authorization': 'Splunk ' + token, 'Content-Type': 'application/json'}
         event_payload = json.dumps({"sourcetype": "json", "index": index, "event": event})
 
         r = requests.post(
-            url=self.url, 
-            headers=event_headers, 
+            url=self.url,
+            headers=event_headers,
             data=event_payload,
             timeout=60,
             verify=False
