c9d4a47717fa376d0932572ec2233e36f609f885
Sean <sean.elliott.11@gmail.com>
2022-01-19 19:54:52 -0700

    fix for best practices

--- actions/get_user.py
+++ actions/get_user.py
@@ -1,4 +1,3 @@
-from nturl2path import url2pathname
 import requests
 import json
 import pprint
@@ -19,14 +18,15 @@ class FindUserAction(Action):
             raise ValueError("No Splunk configuration details found")
         if "splunk_instances" in config:
             if config['splunk_instances'] is None:
-                raise ValueError("'splunk_instances' config defined but empty.")
+                raise ValueError("'splunk_instances' config empty.")
             else:
                 pass
         else:
             raise ValueError("No Splunk configuration details found")
 
     def run(self, instance, userName):
-        requests.packages.urllib3.disable_warnings() 
+        requests.packages.urllib3.disable_warnings()
+
         # Find config details
         if instance:
             splunk_config = self.config['splunk_instances'].get(instance)
@@ -35,47 +35,47 @@ class FindUserAction(Action):
 
         try:
             if splunk_config.get('splunkToken'):
-                host=splunk_config.get('host')
-                port=splunk_config.get('port')
-                splunkToken=splunk_config.get('splunkToken')
-                scheme=splunk_config.get('scheme')
-                verify=splunk_config.get('verify')
+                host = splunk_config.get('host')
+                port = splunk_config.get('port')
+                splunkToken = splunk_config.get('splunkToken')
+                scheme = splunk_config.get('scheme')
+                verify = splunk_config.get('verify')
             else:
-                host=splunk_config.get('host')
-                port=splunk_config.get('port')
-                username=splunk_config.get('username')
-                password=splunk_config.get('password')
-                scheme=splunk_config.get('scheme')
-                verify=splunk_config.get('verify')
-                splunkToken=None
-        
+                host = splunk_config.get('host')
+                port = splunk_config.get('port')
+                username = splunk_config.get('username')
+                password = splunk_config.get('password')
+                scheme = splunk_config.get('scheme')
+                verify = splunk_config.get('verify')
+                splunkToken = None
+
         except BaseException as err:
             raise Exception(
-                "Failed to connect to Splunk Instance {} with error {}".format(splunk_config, err)
-            )
+                "Failed to connect to Splunk"
+                "Instance {} with error {}".format(splunk_config, err))
 
-        base_url = scheme+'://'+host+':'+str(port)
+        base_url = scheme + '://' + host + ':' + str(port)
 
         if not splunkToken:
-            token = requests.get(base_url+"/servicesNS/admin/search/auth/login",
-	                             data={'username':username,
-                                       'password':password, 
-                                       'output_mode':'json'}, verify=verify)
+            token = requests.get(base_url +
+                                 "/servicesNS/admin/search/auth/login",
+                                 data={'username': username,
+                                       'password': password,
+                                       'output_mode': 'json'}, verify=verify)
             json_token = json.loads(token.text)
             session_key = str(json_token['sessionKey'])
 
             headers = {'Accept-Language': 'application/json',
-                       'Authorization': 'Splunk {}'.format(session_key)
-            }
+                       'Authorization': 'Splunk {}'.format(session_key)}
         else:
             headers = {'Accept-Language': 'application/json',
-                       'Authorization': 'Bearer {}'.format(splunkToken)
-            }
-        
+                       'Authorization': 'Bearer {}'.format(splunkToken)}
+
         print(headers)
-        data={'output_mode':'json'}
-        r = requests.get(base_url+'/services/authentication/users/{}'.format(userName),
-                         data=data, 
+        data = {'output_mode': 'json'}
+        r = requests.get(base_url +
+                         '/services/authentication/users/{}'.format(userName),
+                         data=data,
                          headers=headers,
                          verify=verify)
 
