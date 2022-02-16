d868176a7bdb83165d93aa6d572431550c006a26
Rick Kauffman <rick@rickkauffman.com>
2022-02-16 08:43:29 -0700

    added get_hec_token

---
+++ actions/get_hec_token.py
@@ -0,0 +1,53 @@
+import json
+import requests
+import urllib3
+urllib3.disable_warnings()
+import xml.dom.minidom
+
+from lib.base import SplunkBaseAction
+
+__all__ = [
+    'GetHecToken'
+]
+
+#
+class GetHecToken(SplunkBaseAction):
+    """First try to generate a token, if not, get existing token"""
+
+    def run(self, instance):
+        """get hec token"""
+        """Collect instance details from config."""
+
+        if not instance:
+            instance = "default"
+
+        # Get insstance details
+        instance_details = self.instance_details(instance)
+        
+        data = {
+          'name': 'stackstorm',
+          'index': 'main',
+          'indexes': 'main,summary'
+        }
+
+        response = requests.post(instance_details['base_url'] +
+                                 '/servicesNS/nobody/search/data/inputs/http',
+                                 data=data,
+                                 headers=instance_details['headers'],
+                                 verify=instance_details['verify'])
+        if response.status_code != 201:
+            response = requests.get(base_url +
+                                    '/servicesNS/nobody/search/data/inputs/http',
+                                    data=data,
+                                    headers=instance_details['headers'],
+                                    verify=instance_details['verify'])
+
+        dom=xml.dom.minidom.parseString(response.text)
+
+        keys=dom.getElementsByTagName('s:key')
+
+        for n in keys:
+            if n.getAttribute('name') == 'token':
+                myToken = n.childNodes[0].nodeValue
+
+        return (myToken)
